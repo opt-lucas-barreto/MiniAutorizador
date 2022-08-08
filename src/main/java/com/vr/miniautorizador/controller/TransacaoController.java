9455e1c7e4925cab7781aaa29ffe6df165b9bb54
lucas.barreto <!Lucas.123!@#>
2022-08-08 02:18:25 -0300

    acerto no imports

--- src/main/java/com/vr/miniautorizador/controller/TransacaoController.java
+++ src/main/java/com/vr/miniautorizador/controller/TransacaoController.java
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.vr.miniautorizador.dto.CartaoDTO;
 import com.vr.miniautorizador.dto.TransacaoDTO;
 import com.vr.miniautorizador.service.TransacaoService;
 
